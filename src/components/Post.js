17f3349bdcee403d0869006e2e4093e004675482
Fernando Veronez <fernandoveronez_@hotmail.com>
2020-04-21 15:54:33 -0300

    created author component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { View, StyleSheet, Image, Dimensions } from 'react-native'
-
+import Author from './Author'
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container} >
         <Image source={this.props.image} style={styles.image} />
+        <Author email='teste@teste.com' nickname='fulano de tal'/>
       </View>
     )
   }
