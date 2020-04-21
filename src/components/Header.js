05bf2e01b99f1ff6bd338fa127055982b9a07fc0
Fernando Veronez <fernandoveronez_@hotmail.com>
2020-04-21 15:16:34 -0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import {StyleSheet,
+Text,
+View,
+Platform,
+Image
+} from 'react-native'
+
+class Header extends Component {
+  render(){
+    return (
+      <View>
+        
+      </View>
+    )
+  }
+}
