c1a7f3e2e00e91d522f8ce39f69d4bc69cd5fc0a
Fernando Veronez <fernandoveronez_@hotmail.com>
2020-04-25 15:03:57 -0300

    created a comment component

--- App.js
+++ App.js
@@ -6,10 +6,17 @@ import Post from './src/components/Post'
 
 export default class App extends Component{
   render(){
+    const comments=[{
+      nickname: 'Fernando Veronez',
+      comment: 'Excelente foto!'
+    },{
+      nickname: 'Fernando Baltar',
+      comment: 'Porcaria de foto'
+    }]
     return(
       <View style={{flex:1}} >
         <Header />
-        <Post image={require('./assets/imgs/fence.jpg')} />
+        <Post image={require('./assets/imgs/fence.jpg')} comments={comments} />
       </View>
       
     )
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import { View, StyleSheet, Image, Dimensions } from 'react-native'
 import Author from './Author'
-
+import Comments from './Comments'
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container} >
         <Image source={this.props.image} style={styles.image} />
         <Author email='teste@teste.com' nickname='fulano de tal'/>
+        <Comments comments={this.props.comments}/>
       </View>
     )
   }
