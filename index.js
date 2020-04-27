08f88804779f734cb6f23e111cb538d4a169a598
Fernando Veronez <fernandoveronez_@hotmail.com>
2020-04-26 22:14:24 -0300

    configured redux at the app

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 /**
  * @format
  */
-
+import React from 'react'
+import {Provider} from 'react-redux'
 import {AppRegistry} from 'react-native';
 import Navigator from './src/Navigator'
 import {name as appName} from './app.json';
+import storeConfig from './src/store/storeConfig'
 
-AppRegistry.registerComponent(appName, () => Navigator);
+const store = storeConfig()
+const Redux = () =>(
+  <Provider store={store}>
+    <Navigator />
+  </Provider>
+)
+AppRegistry.registerComponent(appName, () => Redux);
